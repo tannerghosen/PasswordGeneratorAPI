3e594108d3d0f41c028545f19bfb1ac718c3317e
Tanner Ghosen <tannerghosen@yahoo.com>
2023-09-19 16:27:01 -0400

    Updated some stuff.

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express()
 const port = 3000
 const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
 const defaultlength = 16;
+const regex = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/;
 
 /* Functions */
-app.get('/password', (req, res) => {
+app.get('/password', (req, res) =>
+{
     var length = 16;
     if (req.query.length != null)
     {
@@ -32,16 +34,40 @@ app.get('/password', (req, res) => {
     }
 })
 
-app.listen(port, () => {
+app.get('/validate', (req, res) =>
+{
+    if (req.query.password != null)
+    {
+        res.send(ValidatePassword(req.query.password));
+    }
+})
+
+
+app.listen(port, () =>
+{
     console.log(`App listening on port ${port}`)
 })
 
-function GeneratePassword(characterset = characters, length = defaultlength) {
+function GeneratePassword(characterset = characters, length = defaultlength)
+{
     var i = 0;
     var password = "";
-    while (i < length) {
+    while (i < length)
+    {
         password += characterset.charAt(Math.floor(Math.random() * characterset.length));
         i++;
     }
     return password;
+}
+
+function ValidatePassword(password)
+{
+    if (!password.match(regex))
+    {
+        return "Password isn't strong. A strong password should be a minimum of 8 characters and contain an uppercase, lowercase, digit, and special character. They also shouldn't have repeating characters.";
+    }
+    else
+    {
+        return "Password is strong.";
+    }
 }
